a70376428b966c69286e3564357318f40dfa71cb
enes <enesooo78@gmail.com>
2022-10-14 21:28:44 +0300

    as

--- F3693-Enes/temawork14-10-22/src/App.js
+++ F3693-Enes/temawork14-10-22/src/App.js
@@ -1,31 +1,13 @@
-
 function App() {
-  const num1= 0
-  const num2 = 100
-  let asal=[]
-  let perfect=[]
-  for(let i =59;i<=num2;i++){
-    let sayac=0
-    for(let a=2;a<i;a++){
-      
-      if(i%a==0){
-        sayac++
-      }
+  
 
-    }
-    if(sayac==0){
-      asal.push(i)
-    }
+  
 
+  return (
+    <div className="App">
 
-  }
-  console.log(asal)
-
-  // return (
-  //   <div className="App">
-      
-  //   </div>
-  // );
+    </div>
+  );
 }
 
 export default App;
