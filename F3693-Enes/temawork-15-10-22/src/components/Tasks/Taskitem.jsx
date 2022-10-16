ae57cdbad7643f5fea4315030b568066bf967c1a
enes <enesooo78@gmail.com>
2022-10-16 20:26:01 +0300

    as

--- F3693-Enes/temawork-15-10-22/src/components/Tasks/Taskitem.jsx
+++ F3693-Enes/temawork-15-10-22/src/components/Tasks/Taskitem.jsx
@@ -5,10 +5,12 @@ import "./task.scss";
 function Taskitem({ taskItem, setTasks }) {
   function onClickdelet(e) {
     let count = 0;
-    for (let i of e.nativeEvent.path[3].childNodes) {
-      if (i.id === e.nativeEvent.path[2].id) {
+    for (let i of data) {
+      if (i.id == e.nativeEvent.path[2].id) {
         setTasks(data.splice(count, 1));
+      
       } else {
+        
         count++;
       }
     }
