a9b1b68543e2c6db5d3bd60451217dab2fce9dfb
enes <enesooo78@gmail.com>
2022-11-18 21:48:55 +0300

    update

--- F3693-Enes/fireblog/src/pages/UpdateBlog/UpdateBlog.jsx
+++ F3693-Enes/fireblog/src/pages/UpdateBlog/UpdateBlog.jsx
@@ -1,5 +1,19 @@
+import { UpdateCon } from "./update-styled";
+
 function UpdateBlog() {
-  return <div>UpdateBlog</div>;
+  return (
+    <UpdateCon>
+        <img src="" alt="" />
+        <h2>Update  Blog</h2>
+        <label htmlFor="title">Title</label>
+        <input id="title" type="text" />
+        <label htmlFor="url">Image URL</label>
+        <input id="url" type="text" />
+        <label htmlFor="content">Content</label>
+        <textarea name="content" id="content" ></textarea>
+        <button>UPDATE</button>
+    </UpdateCon>
+  )
 }
 
 export default UpdateBlog;
