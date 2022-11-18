0005fb774ccac04bf92ab59ecbbca093dbe9ff69
enes <enesooo78@gmail.com>
2022-11-18 22:25:18 +0300

    login

--- F3693-Enes/fireblog/src/pages/Login/Login.jsx
+++ F3693-Enes/fireblog/src/pages/Login/Login.jsx
@@ -1,9 +1,27 @@
-
+import { ImgDiv, LoginBloc, LoginCon, LoginInput } from "./login-stayled";
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <LoginCon>
+      <LoginBloc>
+        <ImgDiv>
+          <img src="images/blok.png" alt="" />
+        </ImgDiv>
+        <h3>Login</h3>
+        <LoginInput>
+          <label htmlFor="email">Email</label>
+          <input placeholder="Email" id="email" type="text" />
+          <label htmlFor="password">Password</label>
+          <input placeholder="Password" id="password" type="text" />
+          <button id="google" >LOGIN</button>
+          <button  >
+            {"WITH"}
+            <img src="images/google.png" alt="" />{" "}
+          </button>
+        </LoginInput>
+      </LoginBloc>
+    </LoginCon>
+  );
 }
 
-export default Login
+export default Login;
