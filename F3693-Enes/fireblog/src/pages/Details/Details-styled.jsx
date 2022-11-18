fc86d9de0cd665d1e56a899111ca604da9447f3d
enes <enesooo78@gmail.com>
2022-11-19 00:44:05 +0300

    details

---
+++ F3693-Enes/fireblog/src/pages/Details/Details-styled.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const DetailsCom = styled.div`
+  min-height: 100vh;
+  width: 100%;
+  background-color: aqua;
+  padding: 0 10rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h2 {
+    font-size: 3rem;
+  }
+  img {
+    background-color: red;
+    width: 100%;
+    /* height: 30rem; */
+  }
+`;
--- F3693-Enes/fireblog/src/pages/Details/Details.jsx
+++ F3693-Enes/fireblog/src/pages/Details/Details.jsx
@@ -1,9 +1,16 @@
+import { DetailsCom } from "./Details-styled"
 
 
 function Details() {
   return (
-    <div>Details</div>
-  )
+    <DetailsCom>
+      <h2>Details</h2>
+      <img
+        src="https://www.ahtapotyazilim.com/wp-content/uploads/2021/10/react-js.jpg"
+        alt=""
+      />
+    </DetailsCom>
+  );
 }
 
 export default Details
