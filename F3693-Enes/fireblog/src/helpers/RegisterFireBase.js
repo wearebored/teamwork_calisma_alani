fb432cffcae77204f7780759901e19b310dadc7b
enes <enesooo78@gmail.com>
2022-11-19 14:07:05 +0300

    regidter

---
+++ F3693-Enes/fireblog/src/helpers/RegisterFireBase.js
@@ -0,0 +1,18 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+function RegisterFireBase() {
+    const auth = getAuth(email,password);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+}
+
+export default RegisterFireBase;
